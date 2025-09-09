f1cf0fb1eff28b91cf320a5d0c0f59546a2f7a5f
nastaaaaa <nastaaa2305@icloud.com>
2025-09-09 22:20:25 +0300

    test 2

--- test/main.cpp
+++ test/main.cpp
@@ -1,10 +1,14 @@
-task 1
-#include <iostream>
-#include <string>
+// task 2
+# include <iostream>
+# include <string>
 
 int main()
-{ 
+{
     long long a, b;
     std::cin >> a >> b;
-    std::cout << a + b;
+    std::cout << a + b << " ";
+    std::cout << a - b << " ";
+    std::cout << a * b << " ";
+    std::cout << a / b << " ";
+
 }
