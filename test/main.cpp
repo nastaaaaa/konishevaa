94ddb8cc76cf43ec13217e639a42175e200f6a91
nastaaaaa <nastaaa2305@icloud.com>
2025-09-09 22:23:13 +0300

    test

--- test/main.cpp
+++ test/main.cpp
@@ -1,4 +1,15 @@
-// task 2
+// task 1
+#include <iostream>
+#include <string>
+
+int main()
+{ 
+    long long a, b;
+    std::cin >> a >> b;
+    std::cout << a + b;
+}
+    
+// // task 2
 # include <iostream>
 # include <string>
 
@@ -11,4 +22,49 @@ int main()
     std::cout << a * b << " ";
     std::cout << a / b << " ";
 
+}
+
+//task 3
+#include <iostream>
+#include <string>
+#include <cmath>
+
+int main()
+{
+    long long a, b;
+    std::cin >> a >> b;
+    std::cout << std::sqrt (a * a + b * b);
+    
+}
+
+// task 4
+#include <iostream>
+#include <string>
+
+int main()
+{
+    long long v, t;
+    std::cin >> v >> t;
+    long long s = (v * t) % 109;
+    std::cout << s;
+}
+
+
+// task 5
+#include <iostream>
+#include <string>
+
+int main()
+{
+    long long n;
+    std::cin >> n;
+    
+    int h = n / 3600;
+    int m = (n % 3600) / 60;
+    int s = n % 60;
+    std::cout << h << ":";
+    if ( m < 10) std::cout << "0";
+    std::cout << m << ":";
+    if (s < 10) std::cout << "0";
+    std::cout << s;
 }
